28b6237112ac1f8fa90c23a60de41c837230acde
KRUTIKA BHATT <krutikabhatt222@gmail.com>
2020-06-07 06:06:41 +0530

    [Uploading File to flip images horizontally]

---
+++ Horizontal_flip.py
@@ -0,0 +1,16 @@
+import cv2
+cam=cv2.VideoCapture(0)
+
+j=0
+while True:
+    x,frame=cam.read()
+    fip=cv2.flip(frame,1)
+    j+=1
+    if(j%2==0):
+        cv2.imshow('Frame',fip)
+    else:
+        cv2.imshow('Frame',frame)
+
+    if cv2.waitKey(2000) &0xFF==ord('a'):
+        break
+    
