4116463faa2c054915845ca05e3aba7e60d9b02e
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2020-06-08 23:21:20 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,5 +6,5 @@ while i<=50:#To store 50 images
     cv2.imwrite('IMG_'+str(i)+'.jpg',x)
     print('IMG-{0}'.format(i))
     i+=1
-    if cv2.waitKey(5000)&0xFF=='a':
+    if cv2.waitKey(5000)&0xFF==ord('a'):
         break
