7e098c7062b2d1ad0fbf7ea942caf7682e75abc6
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2020-06-12 14:14:23 +0530

    Update Ques-2.py

--- Session-4/Ques-2.py
+++ Session-4/Ques-2.py
@@ -1,19 +1,33 @@
-import cv2
-import numpy
-arr=[]
+mport cv2
+import numpy as np
+arr=np.zeros((2,2),int)
+index=0
 img=cv2.imread('Exg.jpg')
 def crop_image(event,x,y,flags,param):
     global arr
-    if event==cv2.EVENT_LBUTTONDOWN:
-        pts=[(x,y)]
-        arr.append((x,y))
-        print(arr)
+    global index
+    if index<2:
+        arr[index]=[x,y]
+        index+=1
+        if np.all(arr):
+            crop_done()
 
-    if len(arr)==2:
-        crop=img[arr[0][1]:arr[1][1],arr[0][0]:arr[1][0]]
-        cv2.imshow('Crop',crop)
-        
+        else:
+            pass
 
+def crop_done():
+    if arr[0][0]<arr[1][0]:
+        if arr[0][1]>arr[1][1]:
+            crop=img[arr[1][1]:arr[0][1],arr[0][0],arr[1][0]]
+
+        else:
+            crop=img[arr[0][1]:arr[1][1],arr[0][0]:arr[1][0]]
+
+    else:
+        crop=img[arr[0][1]:arr[1][1],arr[1][0]:arr[1][1]]
+
+    cv2.imshow('Cropped',crop)
+    cv2.waitKey(0)
 cv2.namedWindow('Frame')
 cv2.setMouseCallback('Frame',crop_image)
 cv2.imshow('Frame',img)
