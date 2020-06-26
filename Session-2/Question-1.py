576d60f41ef9d4bf2f53ba3b5db8c37dd056009a
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2020-06-27 00:51:51 +0530

    Session-2

---
+++ Session-2/Question-1.py
@@ -0,0 +1,11 @@
+import cv2
+
+img=cv2.imread('image.jpg')
+print(img.shape)#Print the dimensions of Image
+
+img=cv2.line(img,(0,0),(img.shape[0],img.shape[1]),(255,0,0),5)
+cv2.imwrite('Strike.jpg')#Saving the striked image
+
+#Displaying image
+cv2.imshow('Frame',img)
+cv2.waitKey(0)
